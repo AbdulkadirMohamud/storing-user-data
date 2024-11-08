1ee8df861954b2f59a4e8d0e397ccc3aa848c975
Abdulkadir <qansax30@hotmail.com>
2024-11-08 22:06:39 +0100

    Add project files.

---
+++ storing user data/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storing_user_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Abdulkadir";
+            string  number = "0123456789";
+            int age = 23;
+            Console.WriteLine(  name);
+            Console.WriteLine(  number);
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
